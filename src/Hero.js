6d005a3d7361d490fedff9f23c4235689e987e52
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2022-06-01 00:41:03 +0530

    Hero AOS -

--- src/Hero.js
+++ src/Hero.js
@@ -3,11 +3,7 @@ import s1 from "./assets/GirlsStip01.png";
 import s2 from "./assets/GirlsStip02.png";
 function Hero() {
   return (
-    <div
-      data-aos="fade-in"
-      data-aos-duration="5000"
-      className="text-white contain py-36 overflow-hidden "
-    >
+    <div className="text-white contain py-36 overflow-hidden ">
       <div className="cont">
         <img src={s1} alt="" className="md:scale-[8] scale-[26]" />
       </div>
